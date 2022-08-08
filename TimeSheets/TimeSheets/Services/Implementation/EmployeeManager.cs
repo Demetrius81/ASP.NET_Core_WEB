3037c97e0a99cf73eb4b0cec0473dfd412d58040
Dmitri <Shizimaniak@gmail.com>
2022-08-08 10:46:18 +0300

    Начал пробрасывать связи между слоями

--- TimeSheets/TimeSheets/Services/Implementation/EmployeeManager.cs
+++ TimeSheets/TimeSheets/Services/Implementation/EmployeeManager.cs
@@ -1,10 +1,47 @@
 ﻿using TimeSheets.Data.Interfaces;
 using TimeSheets.Models;
+using TimeSheets.Models.Dto;
 using TimeSheets.Services.Interfaces;
 
 namespace TimeSheets.Services.Implementation
 {
-    public class EmployeeManager
+    public class EmployeeManager : IEmployeeManager
     {
+        private readonly IUserRepo _employeeRepo;
+
+        public EmployeeManager(IUserRepo employeeRepo)
+        {
+            _employeeRepo = employeeRepo;
+        }
+
+        public Guid AddItem(EmployeeRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteItem(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetItem(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetItem(string Name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<User> GetItems(int skip, int take)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateItem(EmployeeRequest request)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- TimeSheets/TimeSheets/Services/Interfaces/IEmployeeManager.cs
+++ TimeSheets/TimeSheets/Services/Interfaces/IEmployeeManager.cs
@@ -1,6 +1,15 @@
-﻿namespace TimeSheets.Services.Interfaces
+﻿using TimeSheets.Models;
+using TimeSheets.Models.Dto;
+
+namespace TimeSheets.Services.Interfaces
 {
     public interface IEmployeeManager
     {
+        User GetItem(Guid id);
+        User GetItem(string Name);
+        IEnumerable<User> GetItems(int skip, int take);
+        Guid AddItem(EmployeeRequest request);
+        bool UpdateItem(EmployeeRequest request);
+        bool DeleteItem(Guid id);
     }
 }
