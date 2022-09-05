879d4402dc0a1ecb42549c527a8d2da68106ebd0
Dmitri <Shizimaniak@gmail.com>
2022-09-05 15:59:31 +0300

    Добавить авторизацию. Изменить настройки swagger

--- TimeSheets/TimeSheets/Infrastucture/Extentions/ServiceCollectionExtention.cs
+++ TimeSheets/TimeSheets/Infrastucture/Extentions/ServiceCollectionExtention.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using TimeSheets.Data;
 using TimeSheets.Data.Implementation;
 using TimeSheets.Data.Interfaces;
@@ -11,7 +12,7 @@ namespace TimeSheets.Infrastucture.Extentions
 {
     public static class ServiceCollectionExtention
     {
-        public static void ConfigureDataRepositories(this IServiceCollection services)
+        public static IServiceCollection ConfigureDataRepositories(this IServiceCollection services)
         {
             services.AddScoped<IUserRepo, UserRepo>();
             services.AddScoped<IClientRepo, ClientRepo>();
@@ -19,27 +20,33 @@ namespace TimeSheets.Infrastucture.Extentions
             services.AddScoped<IContractRepo, ContractRepo>();
             services.AddScoped<IServiceRepo, ServiceRepo>();
             services.AddScoped<ISheetRepo, SheetRepo>();
+
+            return services;
         }
 
-        public static void ConfigureServiceManagers(this IServiceCollection services)
+        public static IServiceCollection ConfigureServiceManagers(this IServiceCollection services)
         {
             services.AddScoped<IUserManager, UserManager>();
             services.AddScoped<IClientManager, ClientManager>();
             services.AddScoped<IEmployeeManager, EmployeeManager>();
             services.AddScoped<IContractManager, ContractManager>();
             services.AddScoped<ISerrviceManager, ServiceManager>();
-            services.AddScoped<ISheetManager, SheetManager>();           
+            services.AddScoped<ISheetManager, SheetManager>();
+
+            return services;
         }
 
-        public static void ConfigureDBContext(this IServiceCollection services, IConfiguration configuration)
+        public static IServiceCollection ConfigureDBContext(this IServiceCollection services, IConfiguration configuration)
         {
             var connectionString = configuration.GetConnectionString("DefaultConnection");
 
             services.AddDbContext<TimeSheetDbContext>(options =>
-                        options.UseNpgsql(connectionString/*, b=>b.MigrationsAssembly("TimeSheets")*/));
+                        options.UseNpgsql(connectionString, b=>b.MigrationsAssembly("TimeSheets")));
+
+            return services;
         }
 
-        public static void ConfigureAuthentication(this IServiceCollection services, IConfiguration configuration)
+        public static IServiceCollection ConfigureAuthentication(this IServiceCollection services, IConfiguration configuration)
         {
             services.Configure<JwtAccessOptions>(configuration.GetSection("Authentication:JwtAccessOptions"));
 
@@ -55,6 +62,35 @@ namespace TimeSheets.Infrastucture.Extentions
                     x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
                 }).AddJwtBearer(options =>
                     options.TokenValidationParameters = jwtSettings.GetTokenValidationParameters());
+
+            return services;
+        }
+
+        public static IServiceCollection ConfigureBackendSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "TimeSheets", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    BearerFormat = "JWT"
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme()
+                        {
+                            Reference = new OpenApiReference(){Type = ReferenceType.SecurityScheme, Id = "Bearer"}
+                        },
+                        Array.Empty<string>()
+                    }
+                });
+            });
+
+            return services;
         }
     }
 }
--- TimeSheets/TimeSheets/Program.cs
+++ TimeSheets/TimeSheets/Program.cs
@@ -4,29 +4,18 @@ using TimeSheets.Infrastucture.Extentions;
 
 WebApplicationBuilder? builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
-//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
-//builder.Services.AddDbContext<TimeSheetDbContext>(options =>
-//            options.UseNpgsql(connectionString/*, b=>b.MigrationsAssembly("TimeSheets")*/));
-
-builder.Services.ConfigureDBContext(builder.Configuration);
-
-builder.Services.ConfigureAuthentication(builder.Configuration);
-
-builder.Services.ConfigureDataRepositories();
-
-builder.Services.ConfigureServiceManagers();
+builder.Services.ConfigureDBContext(builder.Configuration)
+                .ConfigureAuthentication(builder.Configuration)
+                .ConfigureDataRepositories()
+                .ConfigureServiceManagers()
+                .ConfigureBackendSwagger();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
